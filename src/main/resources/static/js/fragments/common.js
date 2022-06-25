18bad20996156c91873a66be865df7518805e1b0
GIT-SIK <xeno_s@naver.com>
2022-06-25 18:48:37 +0900

    login -> common HTML, CSS, JS 통합

--- src/main/resources/static/js/fragments/common.js
+++ src/main/resources/static/js/fragments/common.js
@@ -1,3 +1,39 @@
-$(function() {
+$(document).ready(function ($) {
 
-})
+
+
+    /* ******** 로그인에 사용할 코드 ******** */
+    $("#bt_view_login").on("click", function (event) {
+        view_login_Modal(true);
+    });
+
+
+    $("body").on("click", function (event) {
+        if (event.target.className == 'backon') {
+            view_login_Modal(false);
+        }
+    });
+
+
+    $(".login_close img").on("click", function (event) {
+        view_login_Modal(false);
+
+    });
+
+    /* ************ 로그인 영역 종료 ************ */
+
+});
+
+
+
+/* 로그인 모달 */
+function view_login_Modal(bool) {
+    if (bool == true) {
+        $(".login_modal").show();
+        $(".backon").show();
+    } else {
+        $(".login_modal").hide();
+        $(".backon").hide();
+    }
+
+}
